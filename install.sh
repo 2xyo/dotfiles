4fe3b42f91d22e498cec9e42b0a87d8e6fd5bb93
Yohann Lepage <yohann@lepage.info>
2013-10-27 17:32:44 +0800

    installOrUpdate

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ contains()
 }
 
 
-installSubtree()
+installOrUpdateSubtree()
 {
 	# 1 name 
 	# 2 path
@@ -24,46 +24,20 @@ installSubtree()
 	then
 		git remote add -f $1 $3
 	else
-		echo "Branch $1 already exists"
+		git fetch $1 master
 	fi
 	
 	if [ ! -d "$2" ]
 	then
 		git subtree add --prefix $2 $1 master --squash
 	else
-		echo "$2 already exist"
-	fi
-}
-
-
-updateSubtree()
-{
-	# 1 name 
-	# 2 path
-	if ! contains "$(git branch -a)" "$1"
-	then
-		git fetch $1 master
-	else
-		echo "Branch $1 already exists"
-	fi
-	
-	if [ ! -d "$2" ]
-	then
 		git subtree pull --prefix $2 $1 master --squash
-	else
-		echo "$2 already exist"
 	fi
 }
 
-installAndUpdateSubtree()
-{
-	installSubtree $1 $2 $3
-	updateSubtree $1 $2
-}
-
-installAndUpdateSubtree bash-it common/bash/bash-it  https://github.com/revans/bash-it.git 
-installAndUpdateSubtree oh-my-zsh common/zsh/oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git 
-installAndUpdateSubtree vim-pathogen common/vim/plugins/vim-pathogen https://github.com/tpope/vim-pathogen.git
+installOrUpdateSubtree bash-it common/bash/bash-it  https://github.com/revans/bash-it.git 
+installOrUpdateSubtree oh-my-zsh common/zsh/oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git 
+installOrUpdateSubtree vim-pathogen common/vim/plugins/vim-pathogen https://github.com/tpope/vim-pathogen.git
 
 
 echo "chsh -s $(which bash) r $(which zsh)"
