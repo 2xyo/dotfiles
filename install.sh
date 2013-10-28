baf22957dfc4379a6bdf72d5b726cea501afbb76
Yohann Lepage <yohann@lepage.info>
2013-10-28 19:05:28 +0800

    typo

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ Or:
 git clone https://github.com/git/git.git
 cd git/contrib/subtree
 make
-sudo install -m 755 git-subtree /usr/lib/git-core"
+sudo install -m 755 git-subtree /usr/lib/git-core
 
 EOF
 
